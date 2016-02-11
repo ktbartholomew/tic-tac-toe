c2439fe9debc406161343baedf2943d43aa30614
Keith Bartholomew <keith.bartholomew@rackspace.com>
2016-02-11 16:52:15 -0600

    adding a footer and a status bar and doing some styling

--- frontend/src/dist/js/app.js
+++ frontend/src/dist/js/app.js
@@ -76,6 +76,19 @@
 
 	var ws = new WebSocket(wsProtocol + window.location.hostname + '/live/');
 	// var ws = new WebSocket(wsProtocol + 'docker:8080/');
+
+	ws.addEventListener('open', function (e) {
+	  requestAnimationFrame(function () {
+	    document.getElementById('butterbar').classList.add('hide');
+	  });
+	});
+
+	ws.addEventListener('close', function (e) {
+	  requestAnimationFrame(function () {
+	    document.getElementById('butterbar').classList.remove('hide');
+	  });
+	});
+
 	module.exports = ws;
 
 	setInterval(function () {
--- frontend/src/src/js/socket.js
+++ frontend/src/src/js/socket.js
@@ -2,6 +2,19 @@ var wsProtocol = (window.location.protocol === 'https:') ? 'wss://' : 'ws://';
 
 var ws = new WebSocket(wsProtocol + window.location.hostname + '/live/');
 // var ws = new WebSocket(wsProtocol + 'docker:8080/');
+
+ws.addEventListener('open', function (e) {
+  requestAnimationFrame(function () {
+    document.getElementById('butterbar').classList.add('hide');
+  });
+});
+
+ws.addEventListener('close', function (e) {
+  requestAnimationFrame(function () {
+    document.getElementById('butterbar').classList.remove('hide');
+  });
+});
+
 module.exports = ws;
 
 setInterval(function () {
